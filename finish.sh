b1c6bb66454efd518c61a82d3c018bef4befcf5d
Rohit Bajaniya <rbajaniya@officebrain.com>
2017-12-08 19:44:48 +0530

    replace travish files

--- finish.sh
+++ finish.sh
@@ -1,7 +1,7 @@
 curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
 -X POST \
-'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/"'"$SERVICE_ID_BACKEND"'"?action=finishupgrade'
+'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s491?action=finishupgrade'
 
 curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
 -X POST \
-'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/'+ $SERVICE_ID_FRONTEND +'?action=finishupgrade'
+'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s490?action=finishupgrade'
