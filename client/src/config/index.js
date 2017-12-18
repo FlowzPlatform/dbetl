087f246af144d7a7999544379e8641614b36030c
bhumilsarvaiya <bhumilsarvaiya@gmail.com>
2017-12-18 12:17:53 +0530

    social media login (facebook and google) enabled

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -3,14 +3,18 @@ var temp = {
   serverURI: 'http://localhost:3034',
   fixedLayout: false,
   hideLogoOnMobile: false,
-  microURI: 'http://ec2-54-88-11-110.compute-1.amazonaws.com/api'
+  microURI: 'http://auth.flowz.com/api',
+  facebookSuccessCallbackUrl: 'http://localhost:8000',
+  loginWithFacebookUrl: 'http://auth.flowz.com/auth/facebook',
+  googleSuccessCallbackUrl: 'http://localhost:8000',
+  loginWithGoogleUrl: 'http://auth.flowz.com/auth/Gplus'
 }
 if (process.env.NODE_ENV !== 'development') {
   temp = {
     serverURI: 'http://api.flowz.com/dbetl',
     fixedLayout: false,
     hideLogoOnMobile: false,
-    microURI: 'http://ec2-54-88-11-110.compute-1.amazonaws.com/api'
+    microURI: 'http://auth.flowz.com/api'
   }
 }
 export default temp
